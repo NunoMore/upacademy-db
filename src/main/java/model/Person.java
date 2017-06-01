2c1fe502f4afa677052ff16058d40e2777bad093
aluno tarde <alunotarde@Altars-Mini-73.lan>
2017-06-01 18:58:37 +0100

    database working

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,11 +1,16 @@
 package model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Person {
-
+	
+	@Id
+	@GeneratedValue
 	private long ID;
+	
 	private String photo;
 	private String academia;
 	private int numTelephone;
