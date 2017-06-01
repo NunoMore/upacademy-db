3aaf96e0cce642fc619db5558f15abf0d4ba06b2
aluno tarde <alunotarde@Altars-Mini-73.lan>
2017-06-01 18:43:05 +0100

    persistence.xml

--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -1,5 +1,17 @@
 package service;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Named("personService")
+@Transactional
+@RequestScoped
 public class PersonService {
+	
+	@PersistenceContext(name="ProjectDataBase")
+	private EntityManager em;
 
 }
