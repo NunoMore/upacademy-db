bf2d46104c1856fa6275985969ed8f7a0900d4be
lpfa123 <l_s_a_123@hotmail.com>
2017-06-07 19:07:00 +0100

    acadeny, academyBean, academyService

---
+++ src/main/java/model/Academy.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class Academy implements Serializable {
+	private static final Long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+
+	private String name;
+	private String description;
+	private String startDate;
+	
+	//@Lob
+	//@Column(name="ACADEMY_PDF", nullable=false, columnDefinition="mediumblob")
+	//private byte[] pdf;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	/*public byte[] getPdf() {
+		return pdf;
+	}
+	public void setPdf(byte[] pdf) {
+		this.pdf = pdf;
+	}*/
+	public static Long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+}
