673abcb6aac30791e9f27c952b896e4baf03f2bc
Andre <andrerei07@gmail.com>
2017-06-26 01:39:43 +0100

    login problema

---
+++ src/main/java/personsAndAcademies/authentication/User.java
@@ -0,0 +1,23 @@
+package personsAndAcademies.authentication;
+
+import personsAndAcademies.model.Entities;
+
+public class User extends Entities {
+
+	
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
