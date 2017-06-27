bd6651f004a1d411456fc5199d35eea595f070f4
lpfa123 <l_s_a_123@hotmail.com>
2017-06-27 23:05:15 +0100

    Ja FUNCEMINA TUDOOO AGORA tenho de tratar do aspecto de algumas coisas

--- src/main/java/personsAndAcademies/view/TableView.java
+++ src/main/java/personsAndAcademies/view/TableView.java
@@ -15,11 +15,15 @@ import personsAndAcademies.model.Person;
 public class TableView implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	
 	private Person selectedPerson; //atributo para guardar person que sera selecionada na tabela do main
 	private List<Person> filteredPersons; //atributo para guardar a seleccao de filtragem
 	
-	private Academy selectedAcademy;
+	private Academy selectedAcademy = new Academy();
 	private List<Academy> filteredAcademies;
+	
+	private AcademyType selectedType;
+	private List<AcademyType> filteredTypes;
 	
 	public AcademyType getSelectedType() {
 		return selectedType;
@@ -37,8 +41,7 @@ public class TableView implements Serializable {
 		this.filteredTypes = filteredTypes;
 	}
 
-	private AcademyType selectedType;
-	private List<AcademyType> filteredTypes;
+
 	
 	public String fillSelectedPerson(Person person){
 		selectedPerson = person;
